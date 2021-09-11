c8dba3c12e55cef31983473db92fdd92797f4f91
Federico Morrone <contact@morrone.dev>
2021-09-11 04:06:38 +0200

    Updated util function

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+// TODO: This functions exits because some values in the api should default to true, I can't recall which ones tho
+#[allow(dead_code)]
 pub(crate) const fn default_true() -> bool {
     true
 }
